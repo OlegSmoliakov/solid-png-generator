552887c2fc86adbf069ca7e5afb9297db6d0e01f
Oleg Smoliakov <OlegSmoliakov@users.noreply.github.com>
2023-03-25 19:49:14 +0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -3,19 +3,22 @@
 set /p location="Enter folder name: " 
 mkdir %location%
 cd %location%
+set /p resolution="Enter resolution separated by a english 'x': " 
+mkdir %resolution%
+cd %resolution%
 
 set /a var = 0 
 :start
 	set /a var+= 1
     	:: step must be calculated manually
-	if %var% EQU 5 goto end
+	if %var% EQU 2 goto end
 		set /a "temp" = %var%
 		cmd /C exit %temp%
 		set "HEX=%=ExitCode%"
         set HEX=%HEX:~6,8%
 		echo %var% %HEX%
 			:: uncomment this to check the funtionality
-		magick convert -size 3840x2160 xc:#%HEX%%HEX%%HEX% %var%_%var%_%var%.png
+		magick convert -size %resolution% xc:#%HEX%%HEX%%HEX% %var%_%var%_%var%.png
 	goto start
 :end
 echo --------------------------------------------
